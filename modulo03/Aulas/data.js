6134a67cea8d23b264da5897d444b93ee3486a05
Lucas Seguessi <lucas.seguessi@gmail.com>
2020-04-21 12:19:04 -0300

    Implemented Dynamic data njk

---
+++ modulo03/Aulas/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    { 
+        id: "xWKRXJ7b9Lw",
+        title: "Java springboot | Lucas Seguessi",
+        duration: "57 min",
+        price: "Free"
+    },
+    { 
+        id: "u99tNt3TZf8",
+        title: "Formulário animado com JS puro e CSS animation | Mayk Brito",
+        duration: "57 min",
+        price: "Free"
+    },
+    { 
+        id: "6WB16wZS61c",
+        title: "Criando pastsa automáticas - Python",
+        duration: "57 min",
+        price: "Free"
+    },
+    { 
+        id: "DDm0M_rZLJo",
+        title: "Formulário para Estoque em C#",
+        duration: "57 min",
+        price: "Free"
+    },
+    { 
+        id: "uonKHztGhko",
+        title: "Infográficos automáticos",
+        duration: "57 min",
+        price: "Free"
+    },
+    { 
+        id: "HKSUPdcK7OU",
+        title: "Front-end com python? Conheça o Brython",
+        duration: "23 min",
+        price: "Free"
+    },
+]
--- modulo03/Aulas/server.js
+++ modulo03/Aulas/server.js
@@ -2,21 +2,37 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require('./data')
 
 server.use(express.static('public'))
 
 server.set('view engine', 'html')
 
 nunjucks.configure('views', {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get("/", function(req, res) {
-    return res.render('about')
+    const about = {
+        avatar_url: "https://avatars0.githubusercontent.com/u/50502905?s=460&u=3e4e23a1095c04b1572305eb2829736e6a1bb98f&v=4",
+        name: "Lucas Seguessi",
+        role: "Desenvolvedor Full-Stack",
+        description: 'Programador front-end, focado em trazer o melhor serviço para todas as empresas. Estudante na <a href="https://rocketseat.com.br" target="_blank">Rockeseat</a>',
+        links: [
+            { name: "GitHub", url: "https://github.com/lseguessi" },
+            { name: "Facebook", url: "https://www.facebook.com/lucas.seguessi/" },
+            { name: "LinkedIn", url: "https://www.linkedin.com/in/lucas-seguessi-801a1045/" }
+        ]
+    }
+
+
+    return res.render('about', {about})
 })
 
 server.get("/portfolio", function(req, res) {
-    return res.render('portfolio')
+
+    return res.render('portfolio', {items: videos})
 })
 
 server.listen(5000, function() {
