07e2f0ca43b36c217e80b99b99a40252e00d117e
Lucas Seguessi <lucas.seguessi@gmail.com>
2020-05-12 10:43:06 -0300

    add to project fields.njk

--- modulo04/Aulas/instructors.js
+++ modulo04/Aulas/instructors.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const Intl = require('Intl')
 const data = require('./data.json')
 const { age } = require('./utils')
 
@@ -18,7 +19,7 @@ exports.show = function(req, res){
         ...foundInstructor,
         age: age(foundInstructor.birth),
         services: foundInstructor.services.split(","),
-        created_at: "",
+        created_at: new Intl.DateTimeFormat('pt-BR').format(foundInstructor.created_at),
     }
 
     return res.render("instructors/show", { instructor })
