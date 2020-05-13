1cacbdaf9c1bf6ac19a246b2ae210b3e0458993d
Lucas Seguessi <lucas.seguessi@gmail.com>
2020-05-13 20:02:31 -0300

    Date UTC and edit instructors add

--- modulo04/Aulas/instructors.js
+++ modulo04/Aulas/instructors.js
@@ -1,19 +1,19 @@
 const fs = require('fs')
 const Intl = require('Intl')
 const data = require('./data.json')
-const { age } = require('./utils')
+const { age, date } = require('./utils')
 
 //show
-exports.show = function(req, res){
+exports.show = function (req, res) {
     //req.params.id = /:id
 
-    const{id} = req.params
+    const { id } = req.params
 
-    const foundInstructor = data.instructors.find(function(instructor){
+    const foundInstructor = data.instructors.find(function (instructor) {
         return instructor.id == id
     })
 
-    if(!foundInstructor) return res.send('Instructor not found!')
+    if (!foundInstructor) return res.send('Instructor not found!')
 
     const instructor = {
         ...foundInstructor,
@@ -26,40 +26,86 @@ exports.show = function(req, res){
 }
 
 //Create
-exports.post = function(req, res){
-        const keys = Object.keys(req.body)
-    
-        for(key of keys){
-            if (req.body[key] == ""){
-                return res.send('Please fill all fields')
-            }
+exports.post = function (req, res) {
+
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+        if (req.body[key] == "") {
+            return res.send('Please fill all fields')
         }
-
-        let {avatar_url, birth, name, services, gender} = req.body
-        
-        birth = Date.parse(birth)
-        const created_at = Date.now()
-        const id = Number(data.instructors.length + 1)
-
-
-        data.instructors.push({
-            id,
-            avatar_url,
-            name, 
-            birth,
-            gender,
-            services,
-            created_at
-        })
-
-        fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
-            if (err) return res.send('Write file error!')
-
-            return res.redirect('/instructors')
-        })
-
-        // return res.send(req.body)
     }
 
+    let { avatar_url, birth, name, services, gender } = req.body
 
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+        if (err) return res.send('Write file error!')
+
+        return res.redirect('/instructors')
+    })
+
+    // return res.send(req.body)
+}
+
+//Edit
+exports.edit = function (req, res) {
+    //req.params.id = /:id
+    const { id } = req.params
+    const foundInstructor = data.instructors.find(function (instructor) {
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send('Instructor not found!')
+
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth) //2000-2-1
+    }
+
+    return res.render('instructors/edit', {instructor })
+}
+
+// put
+exports.put = function(req, res){
+    const { id } = req.body
+    let index = 0
+
+    const foundInstructor = data.instructors.find(function (instructor, foundIndex) {
+        if (id == instructor.id){
+            index = foundIndex
+            return true
+        }
+    })
+
+    if (!foundInstructor) return res.send('Instructor not found!')
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+        if(error) return res.send('Write error!')
+
+        return res.redirect(`/instructors/${id}`)
+    })
+}
 
--- modulo04/Aulas/utils.js
+++ modulo04/Aulas/utils.js
@@ -1,5 +1,4 @@
 module.exports = {
-
     age: function (timestamp) {
         const today = new Date();
         const birthDate = new Date(timestamp)
@@ -10,9 +9,16 @@ module.exports = {
         if (month < 0 || month == 0 && today.getDate() < birthDate.getDate()) {
             age = age - 1
         }
-
         return age
+    },
 
+    date: function(timestamp) {
+        const date = new Date(timestamp)
+
+        const year = date.getUTCFullYear()
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+        const day = `0${date.getUTCDate()}`.slice(-2)
+
+        return `${year}-${month}-${day}`
     }
-
 }
