fcbc801886505daadd85974788d6e73a38f34e45
Lucas Seguessi <lucas.seguessi@gmail.com>
2020-07-13 10:50:00 -0300

    Add filter on instructors

--- modulo05/Aulas/src/app/controllers/instructors.js
+++ modulo05/Aulas/src/app/controllers/instructors.js
@@ -5,10 +5,19 @@ const { age, date } = require('../../lib/utils')
 
 module.exports = {
     index(req, res){
-
-        instructor.all(function(instructors){
-            return res.render('instructors/index', { instructors })
-        })
+        const { filter } = req.query
+        if( filter ){
+            instructor.findBy(filter, function(instructors){
+                return res.render('instructors/index', { instructors, filter })
+            })
+        }else{
+            instructor.all(function(instructors){
+                return res.render('instructors/index', { instructors })
+            })
+        }
+        
+        
+        
         
     },
     create(req, res){
